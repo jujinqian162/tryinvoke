1494e004180ce44a64664699c0af017cda099f2a
jujinqian162 <ju2237210046@gmail.com>
2024-08-11 13:14:53 +0800

    rm main

--- main.cpp
+++ main.cpp
@@ -1,14 +1,5 @@
 
-#include <iostream>
-#include <tuple>
-#include <type_traits>
-#include <utility>
-
-void fun(std::pair<int, int> p){
-
-}
-
 int main(){
-	fun(std::tuple{3, 5});
+	
 	return 0;
 }
